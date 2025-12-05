1c4d6602d266915f8a5032e413552e3caf8df7d5
Valerija Kuzina <lera.kuzina05@gmail.com>
2025-12-05 18:42:22 +0200

    initial
    initial

---
+++ src/test/java/ee/taltech/securefiles/SecurefilesApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.taltech.securefiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurefilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
